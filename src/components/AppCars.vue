38b49e8087c66805a4e593d5a3d3c1005b34b98e
hiki89 <markohitl89@gmail.com>
2018-08-14 20:20:38 +0200

    Zadatak 2

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-
+      <CarsList :cars="cars" />
   </div>
 </template>
 
 <script>
+import CarsList from '../components/CarsList.vue'
+import { cars } from '../services/Cars'
+
 export default {
-  name: 'AppCars',
-  
+    components: {
+        CarsList
+  },
+
+  data() {
+      return {
+          cars: []
+      }
+  },
+
+  beforeRouteEnter(to, from, next) {
+      cars.getAll()
+          .then((response) => {
+              next((vm) => {
+                  vm.cars = response.data
+              })
+          })
+      
+  } 
 }
 </script>
---
+++ src/components/CarsList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul class="list-group list-group-flush" v-for="(car, key) in cars" :key="key">
+            <li class="list-group-item">Brand: {{ car.brand }}</li>
+            <li class="list-group-item">Model: {{ car.model }}</li>
+            <li class="list-group-item">Year: {{ car.year }}</li>
+            <li class="list-group-item">Max Speed: {{ car.maxSpeed }}</li>
+            <li class="list-group-item">Engine: {{ car.engine }}</li>
+            <li class="list-group-item">Number of doors: {{ car.numberOfDoors }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['cars']
+}
+</script>
+
